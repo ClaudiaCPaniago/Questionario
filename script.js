2eeddb8b0cd6900163a304808979d1a36ca00afd
Claudia Paniago <claudiacostapaniago@gmail.com>
2024-02-08 12:24:22 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -97,6 +97,11 @@ const perguntas = [
 const quiz = document.querySelector("#quiz");
 const template = document.querySelector("template");
 
+const corretas = new Set();
+const totalDePerguntas = perguntas.length;
+const mostrarTotal = document.querySelector("#acertos span");
+mostrarTotal.textContent = corretas.size + " de " + totalDePerguntas;
+
 // loop ou laço de repetição
 for (const item of perguntas) {
   const quizItem = template.content.cloneNode(true);
@@ -105,7 +110,21 @@ for (const item of perguntas) {
   for (let resposta of item.respostas) {
     const dt = quizItem.querySelector("dl dt").cloneNode(true);
     dt.querySelector("span").textContent = resposta;
+    dt.querySelector("input").setAttribute(
+      "name",
+      "pergunta-" + perguntas.indexOf(item)
+    );
+    dt.querySelector("input").value = item.respostas.indexOf(resposta);
+    dt.querySelector("input").onchange = (event) => {
+      const estaCorreta = event.target.value == item.correta;
 
+      corretas.delete(item);
+      if (estaCorreta) {
+        corretas.add(item);
+      }
+
+      mostrarTotal.textContent = corretas.size + " de " + totalDePerguntas;
+    };
     quizItem.querySelector("dl").appendChild(dt);
   }
 
